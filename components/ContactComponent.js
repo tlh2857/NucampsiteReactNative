5a4bce784c5b2d43262df2f55b38212a2110f380
Terry Hillis <49683068+tlh2857@users.noreply.github.com>
2020-01-02 17:51:50 -0600

    workshop attempt 1

---
+++ components/ContactComponent.js
@@ -0,0 +1,19 @@
+import React,{Component} from 'react';
+import { View, Text, ScrollView } from 'react-native';
+import { Card } from 'react-native-elements';
+
+
+class Contact extends Component{
+    static navigationOptions = {
+        title: 'Contact Us'
+    }
+render(){
+    return(
+        <ScrollView>
+
+        </ScrollView>
+    )
+}
+}
+
+export default Contact;
