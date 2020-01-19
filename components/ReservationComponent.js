e687acabcd8f8bc5ca013e45b9a2df12cfdfafa1
Terry Hillis <49683068+tlh2857@users.noreply.github.com>
2020-01-19 15:33:48 -0600

    local notification

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -3,6 +3,8 @@ import { Text, View, ScrollView, StyleSheet, Alert,
     Picker, Switch, Button, Modal } from 'react-native';
 import DatePicker from 'react-native-datepicker';
 import * as Animatable from 'react-native-animatable';
+import * as Permissions from 'expo-permissions';
+import {Notifications} from 'expo';
 
 
 
@@ -32,6 +34,28 @@ class Reservation extends Component {
             hikeIn: false,
             date: '',
         });
+    }
+
+    async obtainNotificationPermission(){
+        const permission = await Permissions.getAsync(Permissions.USER_FACING_NOTIFICATIONS);
+        if(permission.status !== 'granted'){
+            const permission = await Permissions.askAsync(Permissions.USER_FACING_NOTIFICATIONS);
+            if(permission.status !== 'granted'){
+                Alert.alert('Permission not granted to show notifications');
+            }
+            return permission;
+        }
+        return permission;
+    }
+
+    async presentLocalNotification(date) {
+        const permission = await this.obtainNotificationPermission();
+        if(permission.status === 'granted'){
+            Notifications.presentLocalNotificationAsync({
+                title: 'Your Campsite Reservation Search',
+                body: `Search for ${date} requested`
+            });
+        }
     }
 
     render() {
@@ -103,7 +127,9 @@ class Reservation extends Component {
                                     },
                                     {
                                         text:"OK",
-                                        onPress:()=> this.resetForm()
+                                        onPress:()=> {
+                                            this.presentLocalNotification(this.state.date)
+                                            this.resetForm()}
                                     }
                                 ],
                                 {cancelable:false}
--- shared/baseUrl.js
+++ shared/baseUrl.js
@@ -1,5 +1,6 @@
-export const baseUrl = 
-'http://192.168.43.156:3000/';
+
 
 const pixeladdress = 'http://192.168.43.156:3000/';
 const apartmentaddress  = 'http://192.168.1.241:3000/';
+
+export const baseUrl = apartmentaddress;
