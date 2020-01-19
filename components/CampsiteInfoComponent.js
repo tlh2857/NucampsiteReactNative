0d1a8e381c0dd2330c7ff235a936d0b37db447c2
Terry Hillis <49683068+tlh2857@users.noreply.github.com>
2020-01-18 19:13:32 -0600

    panresponder

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Text, View, ScrollView, FlatList,
-     Modal, StyleSheet, Button, TextInput } from 'react-native';
+     Modal, StyleSheet, Button, TextInput, Alert, PanResponder } from 'react-native';
 import { Card, Icon, Rating, Input } from 'react-native-elements';
 import {connect} from 'react-redux';
 import {baseUrl} from '../shared/baseUrl';
@@ -23,11 +23,44 @@ const mapDispatchToProps = {
 function RenderCampsite(props) {
    const {campsite} = props;
 
+
+   const recognizeDrag = ({dx}) => (dx<-200?true:false);
+
+   const panResponder = PanResponder.create({
+       onStartShouldSetPanResponder: () => true,
+       onPanResponderEnd: (e,gestureState) => {
+           console.log('pan responder end', gestureState);
+           if(recognizeDrag(gestureState)){
+               Alert.alert(
+                   'Add Favorite',
+                   'Are you sure you wish to add '+campsite.name+' to favorites?',
+                   [
+                       {
+                           text: 'Cancel',
+                           style:'cancel',
+                           onPress: () => console.log('Cancel Pressed')
+                       }, 
+                       {
+                           text: 'OK',
+                           onPress: () => props.favorite? 
+                            console.log('already a favorite'): 
+                            props.markFavorite()
+                       }
+                   ],
+                   {cancelable:false}
+               );
+           }  
+           return true; 
+       }
+   });
+
     if (campsite) {
         return (
-            <Animatable.View animation='fadeInDown' 
+            <Animatable.View 
+            animation='fadeInDown' 
             duration={2000}
-            delay={1000}>
+            delay={1000}
+            {...panResponder.panHandlers}>
             <Card
             featuredTitle={campsite.name}
             image={require('./images/react-lake.jpg')}>
